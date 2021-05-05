a223e807f0fd895cc74a6dcb3ff2e681cb7f952f
Andy Armstrong <andy@hexten.net>
2021-05-05 16:10:55 +0100

    Changes of little account

--- src/mixwith.js
+++ src/mixwith.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 // used by apply() and isApplicationOf()
-const _appliedMixin = '__mixwith_appliedMixin';
+const _appliedMixin = "__mixwith_appliedMixin";
 
 /**
  * A function that returns a subclass of its argument.
@@ -80,11 +80,10 @@ export const hasMixin = (o, mixin) => {
     o = Object.getPrototypeOf(o);
   }
   return false;
-}
-
+};
 
 // used by wrap() and unwrap()
-const _wrappedMixin = '__mixwith_wrappedMixin';
+const _wrappedMixin = "__mixwith_wrappedMixin";
 
 /**
  * Sets up the function `mixin` to be wrapped by the function `wrapper`, while
@@ -119,9 +118,9 @@ export const wrap = (mixin, wrapper) => {
  * @param {MixinFunction} wrapper A wrapped mixin produced by {@link wrap}
  * @return {MixinFunction} The originally wrapped mixin
  */
-export const unwrap = (wrapper) => wrapper[_wrappedMixin] || wrapper;
+export const unwrap = wrapper => wrapper[_wrappedMixin] || wrapper;
 
-const _cachedApplications = '__mixwith_cachedApplications';
+const _cachedApplications = "__mixwith_cachedApplications";
 
 /**
  * Decorates `mixin` so that it caches its applications. When applied multiple
@@ -137,25 +136,26 @@ const _cachedApplications = '__mixwith_cachedApplications';
  * @param {MixinFunction} mixin The mixin to wrap with caching behavior
  * @return {MixinFunction} a new mixin function
  */
-export const Cached = (mixin) => wrap(mixin, (superclass) => {
-  // Get or create a symbol used to look up a previous application of mixin
-  // to the class. This symbol is unique per mixin definition, so a class will have N
-  // applicationRefs if it has had N mixins applied to it. A mixin will have
-  // exactly one _cachedApplicationRef used to store its applications.
+export const Cached = mixin =>
+  wrap(mixin, superclass => {
+    // Get or create a symbol used to look up a previous application of mixin
+    // to the class. This symbol is unique per mixin definition, so a class will have N
+    // applicationRefs if it has had N mixins applied to it. A mixin will have
+    // exactly one _cachedApplicationRef used to store its applications.
 
-  let cachedApplications = superclass[_cachedApplications];
-  if (!cachedApplications) {
-    cachedApplications = superclass[_cachedApplications] = new Map();
-  }
+    let cachedApplications = superclass[_cachedApplications];
+    if (!cachedApplications) {
+      cachedApplications = superclass[_cachedApplications] = new Map();
+    }
 
-  let application = cachedApplications.get(mixin);
-  if (!application) {
-    application = mixin(superclass);
-    cachedApplications.set(mixin, application);
-  }
+    let application = cachedApplications.get(mixin);
+    if (!application) {
+      application = mixin(superclass);
+      cachedApplications.set(mixin, application);
+    }
 
-  return application;
-});
+    return application;
+  });
 
 /**
  * Decorates `mixin` so that it only applies if it's not already on the
@@ -165,10 +165,10 @@ export const Cached = (mixin) => wrap(mixin, (superclass) => {
  * @param {MixinFunction} mixin The mixin to wrap with deduplication behavior
  * @return {MixinFunction} a new mixin function
  */
-export const DeDupe = (mixin) => wrap(mixin, (superclass) =>
-    (hasMixin(superclass.prototype, mixin))
-      ? superclass
-      : mixin(superclass));
+export const DeDupe = mixin =>
+  wrap(mixin, superclass =>
+    hasMixin(superclass.prototype, mixin) ? superclass : mixin(superclass)
+  );
 
 /**
  * Adds [Symbol.hasInstance] (ES2015 custom instanceof support) to `mixin`.
@@ -177,12 +177,12 @@ export const DeDupe = (mixin) => wrap(mixin, (superclass) =>
  * @param {MixinFunction} mixin The mixin to add [Symbol.hasInstance] to
  * @return {MixinFunction} the given mixin function
  */
-export const HasInstance = (mixin) => {
+export const HasInstance = mixin => {
   if (Symbol && Symbol.hasInstance && !mixin[Symbol.hasInstance]) {
     Object.defineProperty(mixin, Symbol.hasInstance, {
       value(o) {
         return hasMixin(o, mixin);
-      },
+      }
     });
   }
   return mixin;
@@ -197,7 +197,7 @@ export const HasInstance = (mixin) => {
  * @param {MixinFunction} mixin The mixin to wrap
  * @return {MixinFunction} a new mixin function
  */
-export const BareMixin = (mixin) => wrap(mixin, (s) => apply(s, mixin));
+export const BareMixin = mixin => wrap(mixin, s => apply(s, mixin));
 
 /**
  * Decorates a mixin function to add deduplication, application caching and
@@ -207,7 +207,7 @@ export const BareMixin = (mixin) => wrap(mixin, (s) => apply(s, mixin));
  * @param {MixinFunction} mixin The mixin to wrap
  * @return {MixinFunction} a new mixin function
  */
-export const Mixin = (mixin) => DeDupe(Cached(BareMixin(mixin)));
+export const Mixin = mixin => DeDupe(Cached(BareMixin(mixin)));
 
 /**
  * A fluent interface to apply a list of mixins to a superclass.
@@ -229,10 +229,9 @@ export const Mixin = (mixin) => DeDupe(Cached(BareMixin(mixin)));
  * @param {Function} [superclass=Object]
  * @return {MixinBuilder}
  */
-export const mix = (superclass) => new MixinBuilder(superclass);
+export const mix = superclass => new MixinBuilder(superclass);
 
 class MixinBuilder {
-
   constructor(superclass) {
     this.superclass = superclass || class {};
   }
--- test/mix-test.js
+++ test/mix-test.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-import {assert} from 'chai';
+import { assert } from "chai";
 
 import {
   apply,
@@ -13,212 +13,244 @@ import {
   Cache,
   DeDupe,
   HasInstance,
-  mix,
-} from '../mixwith';
+  mix
+} from "../mixwith";
 
-suite('mixwith.js', () => {
-
-  suite('apply() and isApplicationOf()', () => {
-
-    test('apply() applies a mixin function', () => {
-      const M = (s) => class extends s {
-        test() {
-          return true;
-        }
-      };
+suite("mixwith.js", () => {
+  suite("apply() and isApplicationOf()", () => {
+    test("apply() applies a mixin function", () => {
+      const M = s =>
+        class extends s {
+          test() {
+            return true;
+          }
+        };
       class Test extends apply(Object, M) {}
       const i = new Test();
       assert.isTrue(i.test());
     });
 
-    test('isApplication() returns true for a mixin applied by apply()', () => {
-      const M = (s) => class extends s {};
+    test("isApplication() returns true for a mixin applied by apply()", () => {
+      const M = s => class extends s {};
       assert.isTrue(isApplicationOf(apply(Object, M).prototype, M));
     });
 
-    test('isApplication() works with wrapped mixins', () => {
-      const M = (s) => class extends s {};
-      const WrappedM = wrap(M, (superclass) => apply(superclass, M));
+    test("isApplication() works with wrapped mixins", () => {
+      const M = s => class extends s {};
+      const WrappedM = wrap(M, superclass => apply(superclass, M));
       assert.isTrue(isApplicationOf(WrappedM(Object).prototype, WrappedM));
     });
 
-    test('isApplication() returns false when it should', () => {
-      const M = (s) => class extends s {};
-      const X = (s) => class extends s {};
+    test("isApplication() returns false when it should", () => {
+      const M = s => class extends s {};
+      const X = s => class extends s {};
       assert.isFalse(isApplicationOf(apply(Object, M).prototype, X));
     });
-
   });
 
-  suite('hasMixin()', () => {
-    test('hasMixin() returns true for a mixin applied by apply()', () => {
-      const M = (s) => class extends s {};
+  suite("hasMixin()", () => {
+    test("hasMixin() returns true for a mixin applied by apply()", () => {
+      const M = s => class extends s {};
 
       assert.isTrue(hasMixin(apply(Object, M).prototype, M));
     });
-
   });
 
-  suite('wrap() and unwrap()', () => {
-
-    test('wrap() sets the prototype', () => {
-      const f = (x) => x*x;
+  suite("wrap() and unwrap()", () => {
+    test("wrap() sets the prototype", () => {
+      const f = x => x * x;
       f.test = true;
-      const wrapper = (x) => f(x);
+      const wrapper = x => f(x);
       wrap(f, wrapper);
       assert.isTrue(wrapper.test);
       assert.equal(f, Object.getPrototypeOf(wrapper));
     });
 
-    test('unwrap() returns the wrapped function', () => {
-      const f = (x) => x*x;
-      const wrapper = (x) => f(x);
+    test("unwrap() returns the wrapped function", () => {
+      const f = x => x * x;
+      const wrapper = x => f(x);
       wrap(f, wrapper);
       assert.equal(f, unwrap(wrapper));
     });
-
   });
 
-  suite('BareMixin', () => {
-
-    test('mixin application is on prototype chain', () => {
-      const M = BareMixin((s) => class extends s {});
+  suite("BareMixin", () => {
+    test("mixin application is on prototype chain", () => {
+      const M = BareMixin(s => class extends s {});
       class C extends M(Object) {}
       const i = new C();
       assert.isTrue(hasMixin(i, M));
     });
 
-    test('methods on mixin are present', () => {
-      const M = BareMixin((s) => class extends s {
-        foo() { return 'foo'; }
-      });
+    test("methods on mixin are present", () => {
+      const M = BareMixin(
+        s =>
+          class extends s {
+            foo() {
+              return "foo";
+            }
+          }
+      );
       class C extends M(Object) {}
       const i = new C();
-      assert.deepEqual(i.foo(), 'foo');
+      assert.deepEqual(i.foo(), "foo");
     });
 
-    test('methods on superclass are present', () => {
-      const M = BareMixin((s) => class extends s {});
+    test("methods on superclass are present", () => {
+      const M = BareMixin(s => class extends s {});
       class S {
-        foo() { return 'foo'; }
+        foo() {
+          return "foo";
+        }
       }
       class C extends M(S) {}
       const i = new C();
-      assert.deepEqual(i.foo(), 'foo');
+      assert.deepEqual(i.foo(), "foo");
     });
 
-    test('methods on subclass are present', () => {
-      const M = BareMixin((s) => class extends s {});
+    test("methods on subclass are present", () => {
+      const M = BareMixin(s => class extends s {});
       class C extends M(Object) {
-        foo() { return 'foo'; }
+        foo() {
+          return "foo";
+        }
       }
       const i = new C();
-      assert.deepEqual(i.foo(), 'foo');
+      assert.deepEqual(i.foo(), "foo");
     });
 
-    test('methods on mixin override superclass', () => {
-      const M = BareMixin((s) => class extends s {
-        foo() { return 'bar'; }
-      });
+    test("methods on mixin override superclass", () => {
+      const M = BareMixin(
+        s =>
+          class extends s {
+            foo() {
+              return "bar";
+            }
+          }
+      );
       class S {
-        foo() { return 'foo'; }
-      }
-      class C extends M(S) {}
-      const i = new C();
-      assert.deepEqual(i.foo(), 'bar');
-    });
-
-    test('methods on mixin can call super', () => {
-      const M = BareMixin((s) => class extends s {
-        foo() { return super.foo(); }
-      });
-      class S {
-        foo() { return 'superfoo'; }
+        foo() {
+          return "foo";
+        }
       }
       class C extends M(S) {}
       const i = new C();
-      assert.deepEqual(i.foo(), 'superfoo');
+      assert.deepEqual(i.foo(), "bar");
     });
 
-    test('methods on subclass override superclass', () => {
-      const M = BareMixin((s) => class extends s {});
+    test("methods on mixin can call super", () => {
+      const M = BareMixin(
+        s =>
+          class extends s {
+            foo() {
+              return super.foo();
+            }
+          }
+      );
       class S {
-        foo() { return 'superfoo'; }
+        foo() {
+          return "superfoo";
+        }
+      }
+      class C extends M(S) {}
+      const i = new C();
+      assert.deepEqual(i.foo(), "superfoo");
+    });
+
+    test("methods on subclass override superclass", () => {
+      const M = BareMixin(s => class extends s {});
+      class S {
+        foo() {
+          return "superfoo";
+        }
       }
       class C extends M(S) {
-        foo() { return 'subfoo'; }
+        foo() {
+          return "subfoo";
+        }
       }
       const i = new C();
-      assert.deepEqual(i.foo(), 'subfoo');
+      assert.deepEqual(i.foo(), "subfoo");
     });
 
-    test('methods on subclass override mixin', () => {
-      const M = BareMixin((s) => class extends s {
-        foo() { return 'mixinfoo'; }
-      });
+    test("methods on subclass override mixin", () => {
+      const M = BareMixin(
+        s =>
+          class extends s {
+            foo() {
+              return "mixinfoo";
+            }
+          }
+      );
       class S {}
       class C extends M(S) {
-        foo() { return 'subfoo'; }
+        foo() {
+          return "subfoo";
+        }
       }
       const i = new C();
-      assert.deepEqual(i.foo(), 'subfoo');
+      assert.deepEqual(i.foo(), "subfoo");
     });
 
-    test('methods on subclass can call super to superclass', () => {
-      const M = BareMixin((s) => class extends s {});
+    test("methods on subclass can call super to superclass", () => {
+      const M = BareMixin(s => class extends s {});
       class S {
-        foo() { return 'superfoo'; }
+        foo() {
+          return "superfoo";
+        }
       }
       class C extends M(S) {
-        foo() { return super.foo(); }
+        foo() {
+          return super.foo();
+        }
       }
       const i = new C();
-      assert.deepEqual(i.foo(), 'superfoo');
+      assert.deepEqual(i.foo(), "superfoo");
     });
-
   });
 
-  suite('DeDupe', () => {
-
-    test('applies the mixin the first time', () => {
-      const M = DeDupe(BareMixin((superclass) => class extends superclass {}));
+  suite("DeDupe", () => {
+    test("applies the mixin the first time", () => {
+      const M = DeDupe(BareMixin(superclass => class extends superclass {}));
       class C extends M(Object) {}
       const i = new C();
       assert.isTrue(hasMixin(i, M));
     });
 
-    test('does\'n apply the mixin the second time', () => {
+    test("does'n apply the mixin the second time", () => {
       let applicationCount = 0;
-      const M = DeDupe(BareMixin((superclass) => {
-        applicationCount++;
-        return class extends superclass {};
-      }));
+      const M = DeDupe(
+        BareMixin(superclass => {
+          applicationCount++;
+          return class extends superclass {};
+        })
+      );
       class C extends M(M(Object)) {}
       const i = new C();
       assert.isTrue(hasMixin(i, M));
       assert.equal(1, applicationCount);
     });
-
   });
 
-  suite('HasInstance', () => {
-
+  suite("HasInstance", () => {
     let hasNativeHasInstance = false;
 
     suiteSetup(() => {
       // Enable the @@hasInstance patch in mixwith.HasInstance
       if (!Symbol.hasInstance) {
-        Symbol.hasInstance = Symbol('hasInstance');
+        Symbol.hasInstance = Symbol("hasInstance");
       }
 
       class Check {
-        static [Symbol.hasInstance](o) { return true; }
+        static [Symbol.hasInstance](o) {
+          return true;
+        }
       }
       hasNativeHasInstance = 1 instanceof Check;
     });
 
-    test('subclasses implement mixins', () => {
-      const M = HasInstance((s) => class extends s {});
+    test("subclasses implement mixins", () => {
+      const M = HasInstance(s => class extends s {});
       class C extends M(Object) {}
       const i = new C();
 
@@ -228,14 +260,12 @@ suite('mixwith.js', () => {
         assert.isTrue(C[Symbol.hasInstance](i));
       }
     });
-
   });
 
-  suite('mix().with()', () => {
-
-    test('applies mixins in order', () => {
-      const M1 = BareMixin((s) => class extends s {});
-      const M2 = BareMixin((s) => class extends s {});
+  suite("mix().with()", () => {
+    test("applies mixins in order", () => {
+      const M1 = BareMixin(s => class extends s {});
+      const M2 = BareMixin(s => class extends s {});
       class S {}
       class C extends mix(S).with(M1, M2) {}
       const i = new C();
@@ -246,34 +276,38 @@ suite('mixwith.js', () => {
       assert.equal(i.__proto__.__proto__.__proto__.__proto__, S.prototype);
     });
 
-    test('mix() can omit the superclass', () => {
-      const M = BareMixin((s) => class extends s {
-        static staticMixinMethod() {
-          return 42;
-        }
+    test("mix() can omit the superclass", () => {
+      const M = BareMixin(
+        s =>
+          class extends s {
+            static staticMixinMethod() {
+              return 42;
+            }
 
-        foo() {
-          return 'foo';
-        }
-      });
+            foo() {
+              return "foo";
+            }
+          }
+      );
       class C extends mix().with(M) {
         static staticClassMethod() {
           return 7;
         }
 
         bar() {
-          return 'bar';
+          return "bar";
         }
       }
       let i = new C();
-      assert.isTrue(hasMixin(i, M), 'hasMixin');
-      assert.isTrue(isApplicationOf(i.__proto__.__proto__, M), 'isApplicationOf');
-      assert.equal('foo', i.foo());
-      assert.equal('bar', i.bar());
+      assert.isTrue(hasMixin(i, M), "hasMixin");
+      assert.isTrue(
+        isApplicationOf(i.__proto__.__proto__, M),
+        "isApplicationOf"
+      );
+      assert.equal("foo", i.foo());
+      assert.equal("bar", i.bar());
       assert.equal(42, C.staticMixinMethod());
       assert.equal(7, C.staticClassMethod());
     });
-
   });
-
 });
